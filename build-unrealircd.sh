0807e0b60eb67ca179e1a2d026e6b8581c1cab7c
sunny <indiasuny000@gmail.com>
2014-11-19 03:47:51 +0530

    stopping make test

--- build-unrealircd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wget https://www.unrealircd.org/downloads/Unreal3.2.10.4.tar.gz
-tar -zxvf Unreal3.2.10.4.tar.gz
--- doubleMagic.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp magic.exp Unreal3.2.10.4/
-cd Unreal3.2.10.4/
-./magic.exp
-make
