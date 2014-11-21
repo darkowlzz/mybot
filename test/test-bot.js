3def96273e1eacedbc4fb3074d1afbe1fcf1ec86
sunny <indiasuny000@gmail.com>
2014-11-22 03:34:30 +0530

    Join multiple channels and send message to them.

--- test/test-bot.js
+++ test/test-bot.js
@@ -2,30 +2,42 @@ var Q = require('q');
 var Bot = require('../bot');
 var should = require('should');
 
-var name1 = 'realbot' + parseInt(Math.random()*100);
-var name2 = 'testbot' + parseInt(Math.random()*100);
+var nick1 = 'realbot' + parseInt(Math.random()*100);
+var nick2 = 'testbot' + parseInt(Math.random()*100);
 
+var config1 = {
+  nick: nick1,
+  channels: ['#hello', '#intro'],
+  server: '127.0.0.1'
+};
+
+var config2 = {
+  nick: nick2,
+  channels: ['#hello', '#intro'],
+  server: '127.0.0.1'
+};
+
+// Create 2 bots, join 2 channels and mention each other.
 describe('test bot', function() {
   it('bot testing', function(done) {
     this.timeout(40000);
 
-    var realbot = new Bot({name: name1});
-    var testbot = new Bot({name: name2});
+    var realbot = new Bot(config1);
+    var testbot = new Bot(config2);
     realbot.connect()
     .then(function(result) {
-      console.log('result is ' + result);
       return testbot.connect();
     })
     .then(function(result) {
-      console.log('2nd result is ' + result);
-
-      realbot.say('hi, my name is ' + realbot.name);
-      testbot.say('hello, my name is ' + testbot.name);
-
       realbot.addMessageListener();
       testbot.addMessageListener();
 
-      testbot.say('hi ' + name1);
+      realbot.say(realbot.channels[0], 'hi, my name is ' + realbot.nick);
+      testbot.say(testbot.channels[0], 'hello, my name is ' + testbot.nick);
+
+      testbot.say(testbot.channels[0], 'hi ' + realbot.nick);
+      testbot.say(testbot.channels[1], 'hi ' + realbot.nick);
+
       return Q.Promise(function(resolve, reject) {
         Q.delay(5000).then(function() {
           resolve('done');
@@ -33,10 +45,8 @@ describe('test bot', function() {
       });
     })
     .then(function(result) {
-      console.log('result after delay ' + result);
-      console.log('testbot buffer: ' + testbot.buffer);
-      testbot.buffer.should.containEql(name2);
-      (1).should.be.exactly(1);
+      testbot.buffer[testbot.channels[0]].should.containEql(testbot.nick);
+      testbot.buffer[testbot.channels[1]].should.containEql(testbot.nick);
       done();
     })
   })
