4cdc894817afe8b74814addd87a0aa7990e197c9
sunny <indiasuny000@gmail.com>
2014-11-22 07:06:43 +0530

    Added channel parting

--- test/test-bot.js
+++ test/test-bot.js
@@ -19,35 +19,74 @@ var config2 = {
 
 // Create 2 bots, join 2 channels and mention each other.
 describe('test bot', function() {
-  it('bot testing', function(done) {
-    this.timeout(40000);
+  var realbot, testbot;
 
-    var realbot = new Bot(config1);
-    var testbot = new Bot(config2);
-    realbot.connect()
+  before(function(done) {
+    this.timeout(45000);
+    realbot = new Bot(config1);
+    testbot = new Bot(config2);
+
+    return Q.try(function() {
+      return realbot.connect();
+    })
     .then(function(result) {
       return testbot.connect();
     })
     .then(function(result) {
       realbot.addMessageListener();
+      realbot.addPartListener();
       testbot.addMessageListener();
+      testbot.addPartListener();
 
       realbot.say(realbot.channels[0], 'hi, my name is ' + realbot.nick);
       testbot.say(testbot.channels[0], 'hello, my name is ' + testbot.nick);
 
+      done();
+    })
+    .catch(function(err) {
+      done(err);
+    });
+  });
+
+  describe('mention test', function() {
+    it('should reply when greeted', function(done) {
+      this.timeout(40000);
       testbot.say(testbot.channels[0], 'hi ' + realbot.nick);
       testbot.say(testbot.channels[1], 'hi ' + realbot.nick);
 
-      return Q.Promise(function(resolve, reject) {
+      Q.Promise(function(resolve, reject) {
         Q.delay(5000).then(function() {
           resolve('done');
         });
+      })
+      .then(function(result) {
+        testbot.buffer[testbot.channels[0]].should.containEql(testbot.nick);
+        testbot.buffer[testbot.channels[1]].should.containEql(testbot.nick);
+        done();
+      })
+      .catch(function(err) {
+        done(err);
       });
-    })
-    .then(function(result) {
-      testbot.buffer[testbot.channels[0]].should.containEql(testbot.nick);
-      testbot.buffer[testbot.channels[1]].should.containEql(testbot.nick);
-      done();
-    })
-  })
+    });
+  });
+
+  describe('part test', function() {
+    it('should listen to part event', function(done) {
+      this.timeout(40000);
+      realbot.part(realbot.channels[0], 'gtg');
+      Q.Promise(function(resolve, reject) {
+        Q.delay(5000).then(function() {
+          resolve('done');
+        });
+      })
+      .then(function(result) {
+        testbot.buffer[testbot.channels[0]].should.containEql(realbot.nick);
+        testbot.buffer[testbot.channels[0]].should.containEql('gtg');
+        done();
+      })
+      .catch(function(err) {
+        done(err);
+      });
+    });
+  });
 });
