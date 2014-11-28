213a8dd218385a04d0d56243e5988e16561fb5c5
sunny <indiasuny000@gmail.com>
2014-11-28 07:43:16 +0530

    Added event listeners.

--- bot.js
+++ bot.js
@@ -116,10 +116,228 @@ Bot.prototype.kill = function() {
   that.client.disconnect('killed');
 };
 
+/*-------------------------------------------------------------------------*/
 
+/**
+ * Adds 'registered' event listener.
+ */
+Bot.prototype.addRegisteredListener = function(callback) {
+  var that = this;
+  that.client.addListener('registered', function(message) {
+    callback.call(that, message);
+  });
+};
+
+/**
+ * Adds 'motd' event listener.
+ */
+Bot.prototype.addMotdListener = function(callback) {
+  var that = this;
+  that.client.addListener('motd', function(motd) {
+    callback.call(that, motd);
+  });
+};
+
+/**
+ * Adds 'names' event listener.
+ */
+Bot.prototype.addNamesListener = function (callback) {
+  var that = this;
+  that.client.addListener('names', function(channel, nick) {
+    callback.call(that, channel, nick);
+  });
+};
+
+/**
+ * Adds 'topic' event listener.
+ */
+Bot.prototype.addTopicListener = function (callback) {
+  var that = this;
+  that.client.addListener('topic', function(channel, topic, nick) {
+    callback.call(that, channel, topic, nick);
+  });
+};
+
+/**
+ * Adds 'join' event listener.
+ */
+/*
+Bot.prototype.addJoinListener = function (callback) {
+  var that = this;
+  that.client.addListener('join', function(channel, nick) {
+    callback.call(that, channel, nick);
+  });
+};
+*/
+
+/**
+ * Adds 'part' event listener.
+ */
+/*
+Bot.prototype.addPartListener = function (callback) {
+  var that = this;
+  that.client.addListener('part', function(channel, nick, reason) {
+    callback.call(that, channel, nick, reason);
+  });
+};
+*/
+
+/**
+ * Adds 'quit' event listener.
+ */
+Bot.prototype.addQuitListener = function (callback) {
+  var that = this;
+  that.client.addListener('quit', function(nick, reason, channels) {
+    callback.call(that, nick, reason, channels);
+  });
+};
+
+/**
+ * Adds 'kick' event listener.
+ */
+Bot.prototype.addKickListener = function (callback) {
+  var that = this;
+  that.client.addListener('kick', function(channel, nick, by, reason) {
+    callback.call(that, channel, nick, by, reason);
+  });
+};
+
+/**
+ * Adds 'kill' event listener.
+ */
+Bot.prototype.addKillListener = function (callback) {
+  var that = this;
+  that.client.addListener('kill', function(nick, reason, channels) {
+    callback.call(that, nick, reason, channels);
+  });
+};
+
+/**
+ * Adds 'message' event listener.
+ */
 Bot.prototype.addMessageListener = function(callback) {
   var that = this;
-  that.client.addListener('message', function(from, to, text) {
-    callback.call(that, from, to, text);
+  that.client.addListener('message', function(nick, to, text) {
+    callback.call(that, nick, to, text);
+  });
+};
+
+/**
+ * Adds 'notice' event listener.
+ */
+Bot.prototype.addNoticeListener = function (callback) {
+  var that = this;
+  that.client.addListener('notice', function(nick, to, text) {
+    callback.call(that, nick, to, text);
+  });
+};
+
+/**
+ * Adds 'ping' event listener.
+ */
+Bot.prototype.addPingListener = function (callback) {
+  var that = this;
+  that.client.addListener('ping', function(server) {
+    callback.call(that, server);
+  });
+};
+
+/**
+ * Adds 'pm' event listener.
+ */
+Bot.prototype.addPmListener = function (callback) {
+  var that = this;
+  that.client.addListener('pm', function(nick, text) {
+    callback.call(that, nick, text);
+  });
+};
+
+/**
+ * Adds 'ctcp' event listener.
+ */
+Bot.prototype.addCtcpListener = function (callback) {
+  var that = this;
+  that.client.addListener('ctcp', function(from, to, text, type) {
+    callback.call(that, from, to, text, type);
+  });
+};
+
+/**
+ * Adds 'nick' event listener.
+ */
+Bot.prototype.addNickListener = function (callback) {
+  var that = this;
+  that.client.addListener('nick', function(oldnick, newnick, channels) {
+    callback.call(that, oldnick, newnick, channels);
+  });
+};
+
+/**
+ * Adds 'invite' event listener.
+ */
+Bot.prototype.addInviteListener = function (callback) {
+  var that = this;
+  that.client.addListener('invite', function(channel, from) {
+    callback.call(that, channel, from);
+  });
+};
+
+/**
+ * Adds '+mode' event listener.
+ */
+Bot.prototype.addPlusModeListener = function (callback) {
+  var that = this;
+  that.client.addListener('+mode', function(channel, by, mode, argument) {
+    callback.call(that, channel, by, mode, argument);
+  });
+};
+
+/**
+ * Adds '-mode' event listener.
+ */
+Bot.prototype.addMinusModeListener = function (callback) {
+  var that = this;
+  that.client.addListener('-mode', function(channel, by, mode, argument) {
+    callback.call(that, channel, by, mode, argument);
+  });
+};
+
+/**
+ * Adds 'whois' event listener.
+ */
+Bot.prototype.addWhoisListener = function (callback) {
+  var that = this;
+  that.client.addListener('whois', function(info) {
+    callback.call(that, info);
+  });
+};
+
+/**
+ * Adds 'channellist_start' event listener.
+ */
+Bot.prototype.addChannellistStartListener = function (callback) {
+  var that = this;
+  that.client.addListener('channellist_start', function() {
+    callback.call(that);
+  });
+};
+
+/**
+ * Adds 'channellist_item' event listener.
+ */
+Bot.prototype.addChannellistItemListener = function (callback) {
+  var that = this;
+  that.client.addListener('channellist_item', function(channel_info) {
+    callback.call(that, channel_info);
+  });
+};
+
+/**
+ * Adds 'channellist' event listener.
+ */
+Bot.prototype.addChannellistItemListener = function (callback) {
+  var that = this;
+  that.client.addListener('channellist', function(channel_list) {
+    callback.call(that, channel_list);
   });
 };
